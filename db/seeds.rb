a7de110da8cbe2ee74056a8a4cc0d3363b1a2c5e
Luca Kuhn <luca.kuhn@hotmail.de>
2019-05-10 15:03:20 +0100

    wiki

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  Article.create(title: Faker::Name.title, content: Faker::Company.catch_phrase)
+end
